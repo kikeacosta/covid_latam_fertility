09920a9bda83550d16bb37b5a604d5474a69ba85
Acosta <kikepaila@gmail.com>
2022-02-23 12:02:45 +0100

    new plots versions and data

--- code/08_fig_monthly_trimestral_pscores.R
+++ code/08_fig_monthly_trimestral_pscores.R
@@ -5,26 +5,6 @@ library(ggridges)
 
 db <- read_rds("data_inter/weekly_excess_confirmed_brazil_colombia.rds")
 
-tx <- 8
-db %>% 
-  group_by(country, div) %>% 
-  mutate(date_peak = date[which.max(dts_excs)] %>% as.numeric()) %>% 
-  ungroup() %>% 
-  ggplot(mapping = aes(x = date, 
-                       y = div,
-                       height = dts_excs,
-                       group = div)) +
-  geom_ridgeline(mapping = aes(y = reorder(div, -date_peak),
-                               fill = as.factor(country)), alpha = .4) +
-  facet_wrap(~ country, ncol = 1, scales = "free_y") +
-  scale_fill_manual(values = c("#2a9d8f", "#e76f51")) +
-  theme_bw()+
-  theme(plot.title = element_text(size = tx + 1),
-        axis.text.y = element_text(size = tx - 1),
-        axis.title.y = element_text(size = tx - 1),
-        legend.position = "none",
-        strip.background = element_rect(fill = "transparent"))
-
 db2 <- 
   db %>% 
   mutate(ini_month = make_date(d  = 1, m = month(date), y = year(date)),
@@ -66,15 +46,22 @@ pop <-
 
 db3 <- 
   dts %>% 
+  mutate(year = year(date)) %>% 
   left_join(bsn) %>% 
-  filter(date >= "2020-01-01") %>% 
+  left_join(pop) %>% 
+  filter(date >= "2020-01-01" & date <= "2021-09-30",
+         div != "Total") %>% 
   mutate(pscore = dts / bsn)
 
 av_pscores <- 
   db3 %>% 
   group_by(country, div) %>% 
   summarise(av_pscore = mean(pscore)) %>% 
-  arrange(country, -av_pscore)
+  arrange(country, -av_pscore) %>% 
+  ungroup() %>% 
+  group_by(country) %>% 
+  mutate(ord = 1:n()) %>% 
+  filter(ord <= 4)
 
 db4 <- 
   db3 %>% 
@@ -82,7 +69,7 @@ db4 <-
     country == "Brazil" & div == "Amazonas" | 
       country == "Brazil" & div == "Rondônia" | 
       country == "Brazil" & div == "Mato Grosso" | 
-      country == "Brazil" & div == "Goiás"  ~ paste0(div, " (Brazil)"),
+      country == "Brazil" & div == "Distrito Federal"  ~ paste0(div, " (Brazil)"),
     country == "Colombia" & div == "Amazonas" | 
       country == "Colombia" & div == "Atlantico" |
       country == "Colombia" & div == "Bogota" |
@@ -94,7 +81,7 @@ cols <-
   c("Amazonas (Brazil)" = "#e41a1c",
     "Rondônia (Brazil)" = "#377eb8",
     "Mato Grosso (Brazil)" = "#4daf4a",
-    "Goiás (Brazil)" = "#984ea3",
+    "Distrito Federal (Brazil)" = "#984ea3",
     "Amazonas (Colombia)" = "#e41a1c",
     "Atlantico (Colombia)" = "#377eb8",
     "Bogota (Colombia)" = "#4daf4a",
@@ -103,39 +90,46 @@ cols <-
 tx <- 8
 
 db4 %>% 
-  filter(date <= "2021-10-01") %>% 
+  filter(date <= "2021-09-30") %>% 
   ggplot(aes(date, pscore)) +
-  geom_boxplot(aes(group = date), outlier.shape = NA)+
-  geom_jitter(aes(date, pscore, col = col_div, alpha = ident, size = ident),
-              width = 5, height = 0)+
+  geom_boxplot(aes(group = date), outlier.shape = NA, 
+               lwd = 0.3, alpha = 0.01, col = "grey50")+
+  geom_jitter(aes(date, pscore, col = col_div, alpha = ident, size = pop),
+              width = 3, height = 0)+
   scale_y_log10(labels = function(x) paste0((x - 1) * 100, "%"), 
-                breaks = c(0.5, 0.75, 1, 1.25, 1.5, 2, 3, 4, 5),
-                limits = c(0.5, 5))+
-  scale_x_date(breaks = seq(ymd('2020-01-01'), ymd('2021-09-01'), by = '3 months'), 
-               date_labels = "%b\n%Y")+
+                breaks = c(0.5, 0.75, 1, 1.25, 1.5, 2, 3, 4, 5))+
+  # scale_x_date(breaks = seq(ymd('2020-01-01'), ymd('2021-09-01'), by = '3 months'), 
+  #              date_labels = "%b\n%Y")+
+  scale_x_date(breaks = seq(ymd('2020-01-01'), ymd('2021-09-01'), by = '1 month'), 
+               date_labels = "%b\n%y")+
   scale_color_manual(values = cols,
                      breaks = c("Amazonas (Brazil)",
                                 "Rondônia (Brazil)",
                                 "Mato Grosso (Brazil)",
-                                "Goiás (Brazil)",
+                                "Distrito Federal (Brazil)",
                                 "Amazonas (Colombia)",
                                 "Atlantico (Colombia)",
                                 "Bogota (Colombia)",
                                 "Magdalena (Colombia)"))+
-  scale_alpha_manual(values = c(1, 0.3), guide = "none")+
-  scale_size_manual(values = c(2, 1.5), guide = "none")+
+  scale_alpha_manual(values = c(0.8, 0.15), guide = "none")+
+  scale_size_continuous(breaks = c(100000, 500000, 1000000, 5000000, 10000000, 40000000),
+                        labels = c("100K", "500K", "1M", "5M", "10M", "40M"))+
+  guides(color = guide_legend(order = 1,
+                              nrow = 2, byrow = TRUE,
+                              override.aes = list(size = 2.5, alpha = 0.8)),
+         size = guide_legend(nrow = 2, byrow = T))+
+  # scale_size_continuous(guide = "none")+
   facet_wrap(~ country)+
   geom_hline(yintercept = 1, linetype = "dashed")+
-  labs(y = "Excess p-score", x = "Date",
-       col = "Subnational\ndivision")+
+  labs(y = "Excess p-score", x = "Month",
+       col = "Subnational\ndivision",
+       size = "Population")+
   theme_bw()+
   theme(legend.position = "bottom",
         legend.title = element_text(hjust = 1),
         legend.text = element_text(size = tx + 2),
         strip.background = element_rect(fill = "transparent"),
-        strip.text = element_text(size = tx + 4)) +
-  guides(color = guide_legend(nrow = 2, byrow = TRUE,
-                              override.aes = list(size = 2.5)))
+        strip.text = element_text(size = tx + 4))
 
 ggsave("figures/pscores_boxplot.png", 
        dpi = 600,
@@ -157,7 +151,8 @@ db5 <-
   mutate(year = year(date)) %>% 
   left_join(bsn) %>% 
   left_join(pop) %>% 
-  filter(date >= "2020-01-01" & date < "2021-08-30") %>%
+  filter(date >= "2020-01-01" & date < "2021-08-30",
+         div != "Total") %>%
   mutate(trim_n = quarter(date),
          trimstr = case_when(year == 2020 & trim_n == 1 ~ "Jan-Mar\n2020",
                              year == 2020 & trim_n == 2 ~ "Apr-Jun\n2020",
@@ -186,21 +181,23 @@ divs_labs <-
   arrange(-pscore) %>% 
   mutate(ord = 1:n()) %>% 
   ungroup() %>% 
-  filter((country == "Colombia" & ord %in% c(1, 2, 33, 34))|
-           (country == "Brazil" & ord %in% c(1, 2, 27, 28))) %>% 
+  filter((country == "Colombia" & ord %in% c(1, 2, 32, 33))|
+           (country == "Brazil" & ord %in% c(1, 2, 26, 27))) %>% 
   select(country, div, trimstr, pscore, ord)
 
 db6 <- 
   db5 %>% 
   left_join(divs_labs) %>% 
-  mutate(col_div = case_when(ord <= 2 ~ "top",
-                             ord > 2 ~ "bottom",
+  mutate(col_div = case_when(ord <= 2 ~ "Highest p-score",
+                             ord > 2 ~ "Lowest p-score",
                              TRUE ~ "other"),
+         col_div = factor(col_div, 
+                          levels = c("Lowest p-score", "Highest p-score", "other")),
          ident = ifelse(col_div == "other", "other", "ident"))
 
 cols <- 
-  c("top" = "#bb3e03",
-    "bottom" = "#0a9396",
+  c("Highest p-score" = "#bb3e03",
+    "Lowest p-score" = "#0a9396",
     "other" = "black")
 
 tx <- 10
@@ -213,7 +210,7 @@ db6 %>%
                   # shape = ident,
                   alpha = ident, 
                   size = pop),
-              width = 0.05, height = 0)+
+              width = 0.025, height = 0)+
   geom_text_repel(data = divs_labs, aes(trimstr, pscore, label = div),
             size = tx / 3, 
             show.legend = FALSE,
@@ -221,34 +218,46 @@ db6 %>%
             box.padding = 0.1,
             direction = "y",
             # nudge_x = 0.1,
-            hjust = -0.3)+
+            hjust = -0.15)+
   scale_y_log10(labels = function(x) paste0((x - 1) * 100, "%"), 
                 breaks = c(0.5, 0.75, 1, 1.25, 1.5, 2, 3, 4, 5))+
-  # scale_x_date(breaks = seq(ymd('2020-01-01'), ymd('2021-09-01'), by = '1 months'),
-  #              minor_breaks = NULL,
-  #              date_labels = "%b\n%Y")+
-  scale_color_manual(values = cols)+
+  scale_color_manual(values = cols, breaks = c("Lowest p-score", "Highest p-score"))+
   scale_alpha_manual(values = c(0.8, 0.2), guide = "none")+
-  # scale_shape_manual(values = c(16, 1), guide = "none")+
-  # scale_size_manual(values = c(2, 1.5), guide = "none")+
-  # guides(color = guide_legend(nrow = 3))+
+  scale_size_continuous(breaks = c(100000, 500000, 1000000, 5000000, 10000000, 40000000),
+                        labels = c("100K", "500K", "1M", "5M", "10M", "40M"))+
+  guides(color = guide_legend(order = 1,
+                              nrow = 1, byrow = TRUE,
+                              override.aes = list(size = 3)),
+         size = guide_legend(nrow = 1))+
   facet_wrap(~ country)+
   geom_hline(yintercept = 1, linetype = "dashed")+
   labs(y = "Excess p-score", 
        x = "Trimester",
-       col = "Excess",
+       col = "Extreme values",
        size = "Population")+
   theme_bw()+
   theme(legend.position = "bottom",
+        legend.title = element_text(size = tx + 3),
+        legend.text = element_text(size = tx + 2),
         axis.text = element_text(size = tx + 2),
         axis.title = element_text(size = tx + 3), 
         strip.background = element_rect(fill = "transparent"),
-        strip.text = element_text(size = tx + 4)) +
-  guides(color = guide_legend(nrow = 1, byrow = TRUE,
-                              override.aes = list(size = 3)))
+        strip.text = element_text(size = tx + 4)) 
 
 ggsave("figures/pscores_boxplot_trim.pdf", 
        dpi = 600,
        w = 20,
        h = 8)
+
+ggsave("figures/pscores_boxplot_trim.png", 
+       dpi = 600,
+       w = 20,
+       h = 8)
+
+trim_out <- 
+  db6 %>% 
+  select(-ord, -col_div, -ident)
+
+write_rds(trim_out, "data_inter/trimestral_excess_confirmed_brazil_colombia.rds")
+write_csv(trim_out, "data_inter/trimestral_excess_confirmed_brazil_colombia.csv")
 
